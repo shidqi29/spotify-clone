21590ecb83160a779d52294c9c3d653be62544f4
shidqi29 <shidqiamirulhaq@gmail.com>
2024-02-07 14:21:21 +0700

    fix(sidebar): sidebar appear on login page

--- src/components/layouts/AppShell/index.tsx
+++ src/components/layouts/AppShell/index.tsx
@@ -5,6 +5,7 @@ import {
   ResizablePanel,
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
+import { useRouter } from "next/router";
 
 type AppShellProps = {
   children: React.ReactNode;
@@ -12,16 +13,23 @@ type AppShellProps = {
 
 // digunakan sebagai layout untuk semua halaman yang membungkus _app.tsx
 export const AppShell = ({ children }: AppShellProps) => {
+  const router = useRouter();
+  const onlyLogin = ["/login"];
+
   return (
     <>
-      <main className={`relative mx-auto flex max-h-screen flex-col py-2`}>
-        <ResizablePanelGroup direction="horizontal">
-          <ResizablePanel defaultSize={23} minSize={23}>
-            <SideBar />
-          </ResizablePanel>
-          <ResizableHandle />
-          <ResizablePanel defaultSize={72}>{children}</ResizablePanel>
-        </ResizablePanelGroup>
+      <main className={`relative mx-auto flex max-h-screen flex-col`}>
+        {onlyLogin.includes(router.pathname) ? (
+          children
+        ) : (
+          <ResizablePanelGroup direction="horizontal" className="py-2">
+            <ResizablePanel defaultSize={23} minSize={23}>
+              <SideBar />
+            </ResizablePanel>
+            <ResizableHandle />
+            <ResizablePanel defaultSize={72}>{children}</ResizablePanel>
+          </ResizablePanelGroup>
+        )}
       </main>
     </>
   );
